cf81d7824de83458bfca4811a1e5e89d7e12ef5e
matejbozic57 <matej.bozic57@gmail.com>
2025-07-03 13:53:14 +0200

    Postavljanje swaggera

--- BACKEND/Program.cs
+++ BACKEND/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +19,15 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/cec1dc005b96b6a3d3962ba063ded2e5b8f9636b/src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIOptionsExtensions.cs#L143
+    //options.ConfigObject.TryItOutEnabled = true;
+    options.EnableTryItOutByDefault();
+});
+
 
 app.MapControllers();
 
