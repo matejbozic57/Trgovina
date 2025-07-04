e07f3e7b2f951782f0f95183a9a56ca23550cb36
matejbozic57 <matej.bozic57@gmail.com>
2025-07-04 13:47:34 +0200

    zavrsetak http rute

---
+++ BACKEND/Controllers/OdjeceController.cs
@@ -0,0 +1,32 @@
+﻿using BACKEND.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OdjeceController : ControllerBase 
+    {
+        private readonly EdunovaContext _context;
+
+        public OdjeceController(EdunovaContext context)
+        {
+            _context = context;
+        }
+            [HttpGet]
+            public IActionResult Get()
+                {
+            try
+            {
+                return Ok(_context.Odjece);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+            }
+        
+    }
+}
